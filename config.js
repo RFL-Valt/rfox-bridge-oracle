2abb4b28b64ab7b8ce00f9b4a6017dffbb9a329f
Afif Alauddin <84457253+afifalauddin@users.noreply.github.com>
2021-10-12 13:02:42 +0800

    Update config.js

--- config.js
+++ config.js
@@ -4,20 +4,19 @@ module.exports = {
   symbol: "RFOX",
   network: "KOVAN",
   eos: {
-    wsEndpoint: "wss://wax.dfuse.eosnation.io/v1/stream", //UNKNOWN, QUERY TO KAEFER/PETE
+    wsEndpoint: "", //STATE RECEIVER WEB SOCKET END POINT
     chainId: "1064487b3cd1a897ce03ae5b6a865651747e2e152090f99c1d19d44e01aea5a4", //TESTNET
-    endpoint: "https://testnet.wax.pink.gg",
-    bridgeContract: "bridge.rfox", //replace with kaefer contract
-    oracleAccount: "afifwaxtest1", //how to get one : provided by oracle
-    privateKey: "PVT_K1_2WWCztiEm3ey6iEeJHtm56FUDPE5Yi4ocBdMba5ViZMgB3rAve", //private key of oracle account
+    endpoint: "https://testnet.wax.pink.gg", //WAX ENDPOINT
+    bridgeContract: "bridge.rfox", //CONTRACT IS THE SAME FOR MAINNET AND TESTNET
+    oracleAccount: "", //ACCOUNT REGISTERED AS ORACLE
+    privateKey: "", //PRIVATE KEY OF ORACLE ACC
   },
   eth: {
-    wsEndpoint: "wss://kovan.infura.io/ws/v3/6e8d4ffb6902406bb556135e343ef61a",
+    wsEndpoint: "wss://kovan.infura.io/ws/v3/6e8d4ffb6902406bb556135e34E3ef61a",
     endpoint: "https://kovan.infura.io/v3/6e8d4ffb6902406bb556135e343ef61a",
-    bridgeContract: "0x59408f7e707233Ab08537736B2a4362997083012", //from Thang
+    bridgeContract: "0x59408f7e707233Ab08537736B2a4362997083012", //BRIDGE SMART CONTRACT ON ETH SIDE
     oracleAccount: "0xeC2f308F7cC266D207af1b1b10a1a3A5966c208b",
-    privateKey:
-      "31e5d2b778c1729a248b284f3f24457da31011b780f779682d9986c3223333f8",
+    privateKey: "", //PRIVATE KEY OF ORACLE ACC
   },
 };
 
