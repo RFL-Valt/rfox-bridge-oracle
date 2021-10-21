5641eda1eb351c5e1843dae3620b16d01aa6b652
Afif <afif@redfoxlabs.io>
2021-10-21 17:44:50 +0800

    Remove ignored files

---
+++ config.example.js
@@ -0,0 +1,21 @@
+module.exports = {
+  precision: 8,
+  ethPrecision: 18,
+  symbol: "RFOX",
+  network: "KOVAN",
+  eos: {
+    wsEndpoint: "",
+    chainId: "1064487b3cd1a897ce03ae5b6a865651747e2e152090f99c1d19d44e01aea5a4",
+    endpoint: "https://wax.pink.gg",
+    bridgeContract: "bridge.rfox",
+    oracleAccount: "",
+    privateKey: "",
+  },
+  eth: {
+    wsEndpoint: "",
+    endpoint: "",
+    bridgeContract: "0x59408f7e707233Ab08537736B2a4362997083012",
+    oracleAccount: "",
+    privateKey: "",
+  },
+};
